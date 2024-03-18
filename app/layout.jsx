d6677168ec3d91340b37b63c084dafe1a6c3988b
TheGoran <42156001+TheGoran@users.noreply.github.com>
2024-03-18 23:44:52 +0000

    removed boilerplate, starting raw lol

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+// This is the main app layout
+//rafce = shortcut for react arrow func component with export at the bottom.
+
+
+import React from 'react'
+
+import '@/assets/styles/global.css'   //the @ means{points} basically start from root of project then dwell into paths lol. 
+
+// Defining global metadata that will apply to each page/component, if they dont have own custom metadata. 
+
+export const metadata = {
+    title: 'TheGoran',
+    description: 'Developer and Digital Artist',
+    keywords: 'Developer, Digital Artist, programmer, frontend, backend, fullstack, react, nextjs, dev ops, software, software solution, hire, build, solution'
+
+}
+
+const MainLayout = ({children}) => {    // we will pass the pages as  children(destructured) prop. 
+  return (
+    <html lang="en">
+        <body>
+            {children}
+        </body>
+    </html>
+  )
+}
+
+export default MainLayout;
