d0f743275e6c57f9e32203e01e07c10ed6234c70
TheGoran <42156001+TheGoran@users.noreply.github.com>
2024-04-08 17:13:52 +0100

    GHActionWorker
    testing

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Home page</h1>
+      <h2>testing CI</h2>
+    </div>
+    
   )
 }
 
