be83d1b22bfcb2228426efb8ff2f76ac1179d3e5
TheGoran <42156001+TheGoran@users.noreply.github.com>
2024-06-05 02:07:18 +0100

    blank
    blank till next

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1>Home page</h1>
-      <h2>CI/CD pipeline   created.   +</h2>
-      <h3>NGINX in place,  proxy from 3000 to 80(single app)  + </h3>
-      <h3>Domain name updated(propagated)  + </h3>
-      <h3>SSL Cert Bot in place.  + </h3>
-      <h3> Replicate for remaining sites, to ensure GHA workers and nginx and CertBot will  works isolated  and as expected - </h3>
+      
     </div>
     
   )
