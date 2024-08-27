ee8346d86d5861bf6e2d10f810c355ea4a577a72
TheGoran <42156001+TheGoran@users.noreply.github.com>
2024-08-27 20:01:53 +0100

    random
    some design mockup

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
 import { PiGithubLogoThin } from "react-icons/pi";
+import { GiCargoCrane } from "react-icons/gi";
+
 
 
 const HomePage = () => {
   console.log("this is  da home page yo")
   return (
     <div className='flex flex-col items-center justify-center  w-screen h-full'>
-      <div className=" p-2 mb-2 w-1/2 justify-center items-enter text-center">
-       <h1 className='text-9xl  font-black'>🏗️ </h1>
-       <h1 className='font-black'>Under Contruction</h1>
-       <div className='p-4 justify-center text-center items-center'>
+      <div className=" p-4 mb-4  justify-center items-enter text-center">
+      <GiCargoCrane size={192}  className='inline'></GiCargoCrane>
+       <h1 className='font-black'>Under Construction</h1>
+       <div className='p-2 justify-center text-center items-center'>
           <p >Check my GitHub repos for prjects and code overview and some demos</p>
         </div>
-       <Link href="https://github.com/TheGoran?tab=repositories" className='text-4xl font-semibold '> <PiGithubLogoThin className='inline' /> GitHub </Link>
+       <Link href="https://github.com/TheGoran?tab=repositories" className='text-4xl font-semibold '> <PiGithubLogoThin size={92} color='orange' className='inline' /> </Link>
       </div>      
     </div>
     
