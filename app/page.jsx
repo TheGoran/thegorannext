7aecd2c84c2160c2d1f163b585e334caf6fd6615
TheGoran <42156001+TheGoran@users.noreply.github.com>
2024-08-27 16:55:15 +0100

    pm2 fixed
    pm2 shall be fixed...whole fuzz might have been outdated pm2 alert DDosed the app it self or something along those lines.

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
     <div className='flex flex-col items-center justify-center md:w-screen sm:w-screen'>
        <h1 className='text-9xl font-black'>🏗️Under Construction</h1>
        <br />
-       <p>Check my GitHub repos for prjects and code overview and some demos.</p>
+       <p>Check my GitHub repos for prjects and code overview and some demos</p>
        <br />
        <Link href="https://github.com/TheGoran?tab=repositories" className='text-6xl font-semibold '> <PiGithubLogoThin className='inline' /> GitHub </Link>
        
